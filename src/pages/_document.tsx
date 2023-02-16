8180ffa13198809463ff6a5c52b137318ca32a0b
vidvidvid <weetopol@gmail.com>
2023-02-16 21:48:27 +0100

    feat: dark

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
   // eslint-disable-next-line class-methods-use-this
   render() {
     return (
-      <Html lang={AppConfig.locale}>
+      <Html lang={AppConfig.locale} data-color-mode="dark">
         <Head />
         <body
           style={{
