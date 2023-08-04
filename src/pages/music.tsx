72c81f88fe0b6415117e29634aa82883e4b5d3ca
vidvidvid <weetopol@gmail.com>
2023-08-04 15:36:02 +0200

    feat: update music

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -1,4 +1,5 @@
-import { Flex, Link, Text } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons'; // Import the ExternalLinkIcon
+import { Box, Flex, Image, Link, Text } from '@chakra-ui/react';
 
 import Crazy from '@/components/3js/Crazy';
 import CountdownTimer from '@/components/CountdownTimer';
@@ -16,28 +17,91 @@ const Music = () => (
       backdropFilter={'blur(5px)'}
       borderRadius="md"
       p={5}
+      h="full"
+      w="full"
     >
+      {/* Add the ExternalLinkIcon next to each link */}
+      <Flex
+        gap={6}
+        mb={6}
+        direction={{
+          base: 'column',
+          md: 'row',
+        }}
+      >
+        <Link
+          href="https://soundcloud.com/malarozamuca?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+          isExternal
+        >
+          <Flex
+            alignItems="center"
+            justifyContent="center"
+            _hover={{
+              transform: 'scale(1.1)',
+              transition: 'all 0.2s',
+            }}
+          >
+            <Image
+              src="/assets/music/soundcloud.svg"
+              w={20}
+              h={20}
+              mr={3}
+              alt="library"
+              filter="invert()"
+            />
+            <ExternalLinkIcon color="pink.200" /> {/* ExternalLinkIcon */}
+          </Flex>
+        </Link>
+        <Link
+          href="https://malarozamuca.bandcamp.com/"
+          isExternal
+          alignSelf="center"
+        >
+          <Flex
+            alignItems="center"
+            _hover={{
+              transform: 'scale(1.1)',
+              transition: 'all 0.2s',
+            }}
+          >
+            <Image
+              src="/assets/music/bandcamp.svg"
+              w={40}
+              mr={3}
+              alt="library"
+              filter="invert()"
+            />
+            <ExternalLinkIcon color="pink.200" /> {/* ExternalLinkIcon */}
+          </Flex>
+        </Link>
+      </Flex>
       <Flex
         w={{
           base: '100%',
           md: '50%',
         }}
         mb={8}
+        justifyContent="center"
       >
-        <iframe
-          style={{
-            borderRadius: '12px',
-          }}
-          src="https://open.spotify.com/embed/album/6YKuxDZZZY6IsSIblhvN0j?utm_source=generator"
-          width="100%"
-          height="152"
-          frameBorder="0"
-          allowFullScreen={false}
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-        ></iframe>
+        <Box>
+          <iframe
+            style={{
+              borderRadius: '12px',
+            }}
+            src="https://open.spotify.com/embed/album/6YKuxDZZZY6IsSIblhvN0j?utm_source=generator"
+            width="100%"
+            height="152"
+            frameBorder="0"
+            allowFullScreen={false}
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
+        </Box>
       </Flex>
-      <Link href="https://soundcloud.com/malarozamuca/" isExternal>
+      <Link
+        href="https://soundcloud.com/malarozamuca/sets/magma-puding"
+        isExternal
+      >
         <Text
           fontSize={{
             base: 30,
